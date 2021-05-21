d1ce070aa6f560f9ad367ab546bbd25fc5f96f82
Me <me@awesome.net>
2021-05-21 14:44:35 +0200

    Read file and get version and line number

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { exec } from '@actions/exec'
+import * as fs from 'fs'
+import path from 'path'
+import { getLineNo, readVersion } from './utils'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+  const githubToken: string = core.getInput('githubtoken', { required: true })
+  const packagePath: string = core.getInput('packagepath', { required: true })
+  const packageContent = fs.readFileSync(
+    path.resolve(__dirname, '../', packagePath),
+    'utf-8'
+  )
+  const version = readVersion(packageContent)
+  const lineNo = getLineNo(packageContent)
+  core.debug(`Token starts with: ${githubToken.slice(0, 2)}`)
+  core.debug(` Version: ${version}, line number ${lineNo}`)
+  let myOutput = ''
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString()
+      }
+    }
   }
+  await exec('git', ['log', `-L${lineNo},${lineNo}:${packagePath}`], options)
+  core.debug(myOutput)
 }
 
 run()
